e59f8d3032d6bf389e80d89d8fd8d50f591c930a
xuhongyuan2014 <xuhongyuan666@163.com>
2014-05-26 21:10:54 +0800

    5.26修改

--- First/src/com/example/first/broadcast/dialogActivity/OutgoingCallDialog.java
+++ First/src/com/example/first/broadcast/dialogActivity/OutgoingCallDialog.java
@@ -5,11 +5,13 @@ import com.example.first.R;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.AlertDialog.Builder;
+import android.app.Dialog;
 import android.content.DialogInterface;
 import android.content.DialogInterface.OnClickListener;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.view.WindowManager;
 import android.widget.EditText;
 import android.widget.Toast;
 
@@ -25,7 +27,7 @@ public class OutgoingCallDialog extends Activity {
 	protected void dialog() {
 
 		AlertDialog.Builder builder = new Builder(OutgoingCallDialog.this);
-
+		
 		builder.setMessage("确认拨打电话吗？");
 
 		builder.setTitle("提示");
@@ -53,7 +55,9 @@ public class OutgoingCallDialog extends Activity {
 			}
 
 		});
-		builder.create().show();
+		Dialog dialog=builder.create();
+		dialog.getWindow().setType(WindowManager.LayoutParams.TYPE_SYSTEM_ALERT);
+		dialog.show();
 
 	}
 
